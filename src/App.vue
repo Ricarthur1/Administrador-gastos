aeaf0830214cf712eece0218610a7bfcc04b419f
Ricardo Alexis Lopez Arthur <19170613@itculiacan.edu.mx>
2024-01-26 13:24:57 -0700

    Creando el steate para gastos y escribiendo sobre el state principal desde el modal con $emit

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,13 @@ const modal = reactive({
 
 const presupuesto = ref(0)
 const disponible = ref(0)
+const gasto = reactive({
+  nombre: '',
+  cantidad: '',
+  categoria: '',
+  id: null,
+  fecha: Date.now()
+})
 
 const definirPresupuesto = (cantidad ) => {
     presupuesto.value = cantidad
@@ -71,6 +78,9 @@ const ocultarModal = () => {
         v-if="modal.mostrar"
         @ocultar-modal="ocultarModal"
         :modal="modal"
+        v-model:nombre="gasto.nombre"
+        v-model:cantidad="gasto.cantidad"
+        v-model:categoria="gasto.categoria"
         />
       </main>
 
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,10 +1,22 @@
 <script setup>
 import cerrarModal from '../assets/img/cerrar.svg'
 
-const emit = defineEmits(['ocultar-modal'])
+const emit = defineEmits(['ocultar-modal', 'update:nombre', 'update:cantidad', 'update:categoria'])
 const props = defineProps({
     modal: {
         type: Object,
+        required: true
+    },
+    nombre: {
+        type: String,
+        required: true
+    },
+    cantidad: {
+        type: [String, Number],
+        required: true
+    },
+    categoria: {
+        type: String,
         required: true
     }
 })
@@ -31,6 +43,8 @@ const props = defineProps({
                     type="text"
                     id="nombre"
                     placeholder="Añade el nombre del gasto"
+                    :value="nombre"
+                    @input="$emit('update:nombre', $event.target.value)"
                     />
                 </div>
 
@@ -40,6 +54,8 @@ const props = defineProps({
                     type="number"
                     id="cantidad"
                     placeholder="Añade la cantidad del gasto, ej. 300"
+                    :value="cantidad"
+                    @input="$emit('update:cantidad', +$event.target.value)"
                     />
                 </div>
 
@@ -47,6 +63,8 @@ const props = defineProps({
                     <label for="categoria">Categoría:</label>
                     <select
                         id="categoria"
+                        :value="categoria"
+                        @input="$emit('update:categoria', $event.target.value)"
                     >
                         <option value=""> -- Seleccione --</option>
                         <option value="ahorro">Ahorro</option>
